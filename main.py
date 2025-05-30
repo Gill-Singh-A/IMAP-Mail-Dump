5ee0c113afc2cde2b8f0da4ec0b6d4b21f4065e6
Gill.Singh.A <amansg22@iitk.ac.in>
2025-05-30 14:42:26 +0530

    Implemented Command-Line Arguments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+from getpass import getpass
 from datetime import date
 from optparse import OptionParser
 from colorama import Fore, Back, Style
@@ -13,6 +14,8 @@ status_color = {
     ' ': Fore.WHITE
 }
 
+imap_port = 993
+
 def display(status, data, start='', end='\n'):
     print(f"{start}{status_color[status]}[{status}] {Fore.BLUE}[{date.today()} {strftime('%H:%M:%S', localtime())}] {status_color[status]}{Style.BRIGHT}{data}{Fore.RESET}{Style.RESET_ALL}", end=end)
 
@@ -23,4 +26,14 @@ def get_arguments(*args):
     return parser.parse_args()[0]
 
 if __name__ == "__main__":
-    pass
+    arguments = get_arguments(('-u', "--user", "user", "Username of EMail"),
+                              ('-s', "--server", "server", "Address of IMAP Server"),
+                              ('-p', "--port", "port", f"Port of IMAP Server (Default={imap_port})"))
+    if not arguments.user:
+        display('-', f"Please Provide a {Back.YELLOW}User{Back.RESET}")
+        exit(0)
+    if not arguments.server:
+        display('-', f"Please Provide a {Back.YELLOW}Server{Back.RESET}")
+        exit(0)
+    arguments.port = imap_port if not arguments.port else int(arguments.port)
+    password = getpass(f"Enter password for {arguments.user}@{arguments.server}:{arguments.port}:")
