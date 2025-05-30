5bfc59df4a01fb2448c2b1d129f57c0deb10bd1e
Gill.Singh.A <amansg22@iitk.ac.in>
2025-05-30 14:47:17 +0530

    Implemented Connection and Authentication to IMAP Server

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #! /usr/bin/env python3
 
-from getpass import getpass
+import sys, imaplib, email
+from pathlib import Path
 from datetime import date
+from getpass import getpass
+from email.parser import Parser
 from optparse import OptionParser
-from colorama import Fore, Back, Style
 from time import strftime, localtime
+from colorama import Fore, Back, Style
 
 status_color = {
     '+': Fore.GREEN,
@@ -36,4 +39,18 @@ if __name__ == "__main__":
         display('-', f"Please Provide a {Back.YELLOW}Server{Back.RESET}")
         exit(0)
     arguments.port = imap_port if not arguments.port else int(arguments.port)
-    password = getpass(f"Enter password for {arguments.user}@{arguments.server}:{arguments.port}:")
+    password = getpass(f"Enter password for {arguments.user}@{arguments.server}:{arguments.port} => ")
+
+    display(':', f"Connecting to Server {Back.MAGENTA}{arguments.server}:{arguments.port}{Back.RESET}")
+    try:
+        Mailbox = imaplib.IMAP4_SSL(arguments.server, port=arguments.port)
+    except:
+        display('-', f"Can't Reach IMAP Server {Back.MAGENTA}{arguments.server}:{arguments.port}{Back.RESET}")
+        exit(0)
+    display(':', f"Authenticating User {Back.MAGENTA}{arguments.user}{Back.RESET}  @ IMAP Server {Back.MAGENTA}{arguments.server}:{arguments.port}{Back.RESET}")
+    try:
+        Mailbox.login(arguments.user, password)
+    except:
+        display('-', f"Authentication Failed for User {Back.MAGENTA}{arguments.user}{Back.RESET} @ IMAP Server {Back.MAGENTA}{arguments.server}:{arguments.port}{Back.RESET}")
+        exit(0)
+    display('+', f"User {Back.MAGENTA}{arguments.user}{Back.RESET} Authenticated @ {Back.MAGENTA}{arguments.server}:{arguments.port}{Back.RESET}")
